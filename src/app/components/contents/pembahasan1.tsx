5866a74062bd3db5bda4ea32fa56d782f4afc719
arfani <arfanihidayat@gmail.com>
2024-02-25 00:54:06 +0800

    fix eslint error

--- src/app/components/contents/pembahasan1.tsx
+++ src/app/components/contents/pembahasan1.tsx
@@ -8,9 +8,9 @@ export const Pembahasan1: FC = () => {
             <h2 className='font-semibold'>2 Cara Penentuan Awal Bulan :</h2>
             <ol className='list-decimal list-inside'>
                 <li>Melihat Hilal</li>
-                <li>Menyempurnakan Bulan Sya'ban menjadi 30 hari</li>
+                <li>Menyempurnakan Bulan {"Sya'ban"} menjadi 30 hari</li>
             </ol>
-            <p><strong>Adapun Melihat hilal :</strong> Maka dengan cara seorang muslim melihatnya sendiri, atau dikabarkan oleh seorang yang terpercaya bahwa orang tersebut melihat hilal. Dalilnya adalah hadits Nabi <span className='text-[8px]'>ﷺ</span> ( "Berpuasalah jika kalian melihat hilal." ), dan firman Allah <span className='text-[8px]'>ﷻ</span> (( "Maka siapa yang berada pada Bulan itu (Ramadhan), maka berpuasalah." [Al-Baqarah:185])).</p>
+            <p><strong>Adapun Melihat hilal :</strong> Maka dengan cara seorang muslim melihatnya sendiri, atau dikabarkan oleh seorang yang terpercaya bahwa orang tersebut melihat hilal. Dalilnya adalah hadits Nabi <span className='text-[8px]'>ﷺ</span> ( {"Berpuasalah jika kalian melihat hilal."} ), dan firman Allah <span className='text-[8px]'>ﷻ</span> (( {"Maka siapa yang berada pada Bulan itu (Ramadhan), maka berpuasalah."} [Al-Baqarah:185])).</p>
         </>
     )
 }
