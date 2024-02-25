5a649ab74c7f3dad32b24f65333ac7d09fe53ec1
arfani <arfanihidayat@gmail.com>
2024-02-25 15:55:40 +0800

    perkecil symbol arabic

--- src/app/components/contents/pembahasan1.tsx
+++ src/app/components/contents/pembahasan1.tsx
@@ -11,17 +11,17 @@ export const Pembahasan1: FC = () => {
                 <li>Menyempurnakan Bulan {"Sya'ban"} menjadi 30 hari</li>
             </ol>
             <p>
-                <strong>Adapun melihat hilal :</strong> maka dengan cara seorang muslim melihatnya sendiri, atau dikabarkan oleh seorang yang terpercaya bahwa orang tersebut melihat hilal. Dalilnya adalah hadits Nabi ﷺ ( {"Berpuasalah jika kalian melihat hilal."} ), dan firman Allah ﷻ (( {"Maka siapa yang berada pada Bulan itu (Ramadhan), maka berpuasalah."} [Al-Baqarah:185])).
+                <strong>Adapun melihat hilal :</strong> maka dengan cara seorang muslim melihatnya sendiri, atau dikabarkan oleh seorang yang terpercaya bahwa orang tersebut melihat hilal. Dalilnya adalah hadits Nabi <span className='text-[9px]'>ﷺ</span> ( {"Berpuasalah jika kalian melihat hilal."} ), dan firman Allah <span className='text-[9px]'>ﷻ</span> (( {"Maka siapa yang berada pada Bulan itu (Ramadhan), maka berpuasalah."} [Al-Baqarah:185])).
             </p>
 
             <p>
-                <strong>Dan adapun menyempurnakan bulan {"Sya'ban"} :</strong> berdasarkan hadits Nabi ﷺ yang saabiq ( {"Maka jika langit mendung, maka sempurnakan bilangan bulan Sya'ban menjadi 30 hari."} ), dan ini seperti yang terjadi pada tahun ini <em>- tahun 1436 -</em> tidak terlihat hilal maka kita menyempurnakan bilangan bulan {"Sya'ban"} menjadi 30 hari.
+                <strong>Dan adapun menyempurnakan bulan {"Sya'ban"} :</strong> berdasarkan hadits Nabi <span className='text-[9px]'>ﷺ</span> yang saabiq ( {"Maka jika langit mendung, maka sempurnakan bilangan bulan Sya'ban menjadi 30 hari."} ), dan ini seperti yang terjadi pada tahun ini <em>- tahun 1436 -</em> tidak terlihat hilal maka kita menyempurnakan bilangan bulan {"Sya'ban"} menjadi 30 hari.
             </p>
 
             <h2 className='font-semibold'>Adapun metode hisab, maka tidak ada dasarnya dan tidak benar untuk berpegang padanya baik untuk berpuasa maupun idul fitri berdasarkan nash dan ijmaq  :</h2>
 
             <p>
-                <strong>Adapun berdasarkan nash  :</strong> maka hadits Nabi ﷺ ( {"Berpuasalah jika kalian melihat hilal."} ), dan Allah ﷻ menginginkan ibadah ini menjadi ibadah yang tidak memberatkan kita, ibadah yang mudah untuk dilaksanakan, jika kita melihat hilal maka kita berpuasa walhamduillah, dan jika tidak maka kita sempurnakan bulan {"Sya'ban"} menjadi 30 hari. Dan nabi ﷺ bersabda ( {"Binasalah orang yang berlebih-lebihan."} ), dan yang berpegang dengan metode hisab maka tidak diragukan dia benar-benar termasuk orang yang berlebih-lebihan di dalam agama Allah ﷻ.
+                <strong>Adapun berdasarkan nash  :</strong> maka hadits Nabi <span className='text-[9px]'>ﷺ</span> ( {"Berpuasalah jika kalian melihat hilal."} ), dan Allah <span className='text-[9px]'>ﷻ</span> menginginkan ibadah ini menjadi ibadah yang tidak memberatkan kita, ibadah yang mudah untuk dilaksanakan, jika kita melihat hilal maka kita berpuasa walhamduillah, dan jika tidak maka kita sempurnakan bulan {"Sya'ban"} menjadi 30 hari. Dan nabi <span className='text-[9px]'>ﷺ</span> bersabda ( {"Binasalah orang yang berlebih-lebihan."} ), dan yang berpegang dengan metode hisab maka tidak diragukan dia benar-benar termasuk orang yang berlebih-lebihan di dalam agama Allah <span className='text-[9px]'>ﷻ</span>.
             </p>
             <p>
                 <strong>Adapun berdasarkan ijmaq  :</strong> maka telah dinukil ijmaq yang berkaitan dengannya <em>- yaitu berkaitan dengan tidak menggunakan metode hisab di dalam berpuasa maupun idul fitri -</em> beberapa dari ahli ilmu diantaranya Al Qurofi Al Maliki rahimahullah, dan juga Syaikhul Islam Ibnu Taimiyah rahimahullah, dan yang lainnya.
